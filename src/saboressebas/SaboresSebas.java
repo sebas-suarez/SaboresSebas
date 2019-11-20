737cd949fc3c253b62a05fb712314349abbb14d3
sebas-suarez <sebasuarez725@gamil.com>
2019-11-19 21:51:05 -0300

    primera version

---
+++ src/saboressebas/SaboresSebas.java
@@ -0,0 +1,16 @@
+
+package saboressebas;
+
+
+public class SaboresSebas {
+
+
+    public static void main(String[] args) {
+       
+        String[] sabores = { "Dulce","Salado","Amargo","Acido","Umami","Picante","Astringente","Starchy","Adiposo"};
+        for(int i = 0; i < 6; i++){
+          System.out.println("Sabor:" + sabores[i]);
+        }
+    }
+    
+}
