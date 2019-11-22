04c85df88553cd2c44e5bf46bef1164180960b69
sebas-suarez <sebasuarez725@gamil.com>
2019-11-21 21:20:09 -0300

    Sabores 2.0

--- src/saboressebas/SaboresSebas.java
+++ src/saboressebas/SaboresSebas.java
@@ -8,10 +8,16 @@ public class SaboresSebas {
     public static void main(String[] args) {
        
         String[] sabores = { "Dulce","Salado","Amargo","Acido","Umami","Picante","Astringente","Starchy","Adiposo"};
+        String[] sabores2 = {"feo","asco","muyfeo","to el asco","horrible"};
         for(int i = 0; i < 9; i++){
           
           System.out.println("Sabor: " + sabores[i]);
+          System.out.println("otros sabores:" + sabores2[i]);
         }
+        
+        
+        
+        
     }
     
 }
